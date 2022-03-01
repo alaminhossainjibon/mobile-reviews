38b84df9c15d04989a0ec9d2e6bce177f4d863b0
alaminhossainjibon <mdalamin7622@gmail.com>
2022-03-02 02:16:21 +0600

    phone feature add

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const searchPhone = async () => {
+// search phone
+const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // clear data
@@ -7,65 +8,44 @@ const searchPhone = async () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayPhone(data.data))
+        .then(data => displayPhone(data.data.slice(0, 20)))
 }
 // search result
 const displayPhone = data => {
     const searchResult = document.getElementById('search-result')
     //clear data
     searchResult.textContent = '';
-
+    // phone of names, brand names, and model names
     data.forEach(data => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-            <div onclick="loadPhoneDetail(${data.idPhone})" class="card h-100">
-                    <img src="${data.image}" class="card-img-top" alt="...">
+    <div (${data.idphone})" class="card h-100">
+            <img src="${data.image}" class="card-img-top" alt="...">
 
-                <div class="card-body">
-                     <h5 class="card-title">${data.brand}</h5>
-                    <p>${data.phone_name}</p>
-                    <p>${data.slug}</p>
+        <div class="card-body">
+            <h5 class="card-title">Brand: ${data.brand}</h5>
+            <p>Name: ${data.phone_name}</p>
+            <p>Model: ${data.slug}</p>
 
-                    <div class="all-button">
-                    <button class="btn btn-outline-success" type="button" id="button-search">Feaser</button>
-                    </div>
-
-                </div>
-            </div>
+            <button onclick="details('${data.idphone}')" class="btn btn-outline-success" type="button">Featur</button>
+        </div>
+    </div>
         `;
         searchResult.appendChild(div);
     });
 }
 
-
-//load phone details
-const loadPhoneDetail = async id => {
-    const url = `https://openapi.programming-hero.com/api/phone/${id}`
-
-    const res = await fetch(url);
-    const data = await res.json();
-    displayPhoneDetail(data.data[0]);
+// //////phone featur added
+const details = (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => setDetails(data.data[0]));
 }
-// phone info
-const displayPhoneDetail = data => {
-    const phoneDetails = document.getElementById('phone-details');
+const setDetails = (id) => {
+    document.getElementById('search-field').innerHTML = `
+    <p>${data.mainFeatures.storage}</p>
+    `;
 
-    phoneDetails.textContent = '';
-
-    const div = document.createElement('div');
-
-    div.classList.add('card');
-
-    div.innerHTML = `
-    <img src="${data.image}" class="card-img-top" alt="...">
-
-<div class="card-body">
-     <h5 class="card-title">${data.brand}</h5>
-    <p>${data.phone_name}</p>
-    <p>${data.slug}</p>
-
-</div>
-`;
-    phoneDetails.appendChild(div);
 }
