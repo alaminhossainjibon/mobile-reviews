bb81b6d013c817eba94edda92bedf1f193bd3a3f
alaminhossainjibon <mdalamin7622@gmail.com>
2022-03-01 13:33:09 +0600

    load phone detail

--- js/app.js
+++ js/app.js
@@ -1,16 +1,32 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // console.log(searchText)
     searchField.value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    // console.log(url)
     fetch(url)
         .then(res => res.json())
-        .then(data => displayPhone(data.phones))
+        .then(data => displayPhone(data.data))
 }
 // search result
-const displayPhone = phones => {
+const displayPhone = data => {
     const searchResult = document.getElementById('search-result')
     searchResult.textContent = '';
+
+    data.forEach(data => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div onclick="loadPhoneDetail(${data.id})" class="card h-100">
+                    <img src="${data.image}" class="card-img-top" alt="...">
+
+                <div class="card-body">
+                     <h5 class="card-title">${data.brand}</h5>
+                    <p>${data.phone_name}</p>
+                    <p>${data.slug}</p>
+
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    });
 }
